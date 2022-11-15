5874564f18062c2fbb122ff42d0befd1ab8c8aa0
leon <daricoder@gmail.com>
2022-11-15 02:24:50 -0500

    additional configuration for react router for gh-pages

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <CarritoContext>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route exact path="/" element={<App />} />
           <Route exact path="/carritoDetail" element={<CarritoDetail />} />
